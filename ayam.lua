3fce52f6ecdac9f35946aa8109f69cda4974ae33
luxxidroblox-code <luxxidroblox@gmail.com>
2026-09-12 23:49:18 +0700

    Update ayam.lua

--- ayam.lua
+++ ayam.lua
@@ -585,7 +585,7 @@ local function rollUntilTarget(remote, etc, hrp)
     return false
 end
 
-local TWEEN_DURATION = 45
+local TWEEN_DURATION = 46
 
 local function runAutofarm()
     StartMoney        = getCleanMoney()
